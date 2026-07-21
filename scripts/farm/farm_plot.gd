6d418c822e4a1b2fd2e690dc41e8e4b646b821e6
Rob F <robfernandez06@gmail.com>
2026-07-21 11:40:43 -0400

    TASK-015: center FarmPlot crop visuals

--- scripts/farm/farm_plot.gd
+++ scripts/farm/farm_plot.gd
@@ -10,6 +10,9 @@ const GROWING_SHADOW_TEXTURE := preload("res://assets/sprites/crops/sunwheat_sha
 const GROWING_COLOR_TEXTURE := preload("res://assets/sprites/crops/sunwheat_crop_growing_256.png")
 const READY_SHADOW_TEXTURE := preload("res://assets/sprites/crops/sunwheat_shadow_ready_256.png")
 const READY_COLOR_TEXTURE := preload("res://assets/sprites/crops/sunwheat_crop_ready_256.png")
+const EMPTY_CROP_POSITION := Vector2(-128.0, -232.5)
+const GROWING_CROP_POSITION := Vector2(-127.0, -232.5)
+const READY_CROP_POSITION := Vector2(-140.0, -232.5)
 
 @export var initial_visual_state: VisualState = VisualState.EMPTY
 
@@ -26,22 +29,22 @@ func _ready() -> void:
 func set_visual_state(state: VisualState) -> void:
 	match state:
 		VisualState.EMPTY:
-			_crop_shadow.texture = null
-			_crop_color.texture = null
-			_crop_shadow.visible = false
-			_crop_color.visible = false
+			_apply_crop_visual(null, null, EMPTY_CROP_POSITION, false)
 		VisualState.GROWING:
-			_crop_shadow.texture = GROWING_SHADOW_TEXTURE
-			_crop_color.texture = GROWING_COLOR_TEXTURE
-			_crop_shadow.visible = true
-			_crop_color.visible = true
+			_apply_crop_visual(GROWING_SHADOW_TEXTURE, GROWING_COLOR_TEXTURE, GROWING_CROP_POSITION, true)
 		VisualState.READY:
-			_crop_shadow.texture = READY_SHADOW_TEXTURE
-			_crop_color.texture = READY_COLOR_TEXTURE
-			_crop_shadow.visible = true
-			_crop_color.visible = true
+			_apply_crop_visual(READY_SHADOW_TEXTURE, READY_COLOR_TEXTURE, READY_CROP_POSITION, true)
 	_visual_state = state
 
 
 func get_visual_state() -> VisualState:
 	return _visual_state
+
+
+func _apply_crop_visual(shadow_texture: Texture2D, color_texture: Texture2D, pair_position: Vector2, is_visible: bool) -> void:
+	_crop_shadow.texture = shadow_texture
+	_crop_color.texture = color_texture
+	_crop_shadow.position = pair_position
+	_crop_color.position = pair_position
+	_crop_shadow.visible = is_visible
+	_crop_color.visible = is_visible
--- tests/farm_plot_visual_state_test.gd
+++ tests/farm_plot_visual_state_test.gd
@@ -7,7 +7,11 @@ const GROWING_SHADOW_PATH := "res://assets/sprites/crops/sunwheat_shadow_growing
 const GROWING_COLOR_PATH := "res://assets/sprites/crops/sunwheat_crop_growing_256.png"
 const READY_SHADOW_PATH := "res://assets/sprites/crops/sunwheat_shadow_ready_256.png"
 const READY_COLOR_PATH := "res://assets/sprites/crops/sunwheat_crop_ready_256.png"
-const LAYER_POSITION := Vector2(-128.0, -232.5)
+const BASE_POSITION := Vector2(-128.0, -232.5)
+const EMPTY_CROP_POSITION := Vector2(-128.0, -232.5)
+const GROWING_CROP_POSITION := Vector2(-127.0, -232.5)
+const READY_CROP_POSITION := Vector2(-140.0, -232.5)
+const SOIL_CONTACT_Y := -232.5
 
 var _failures: Array[String] = []
 
@@ -49,10 +53,15 @@ func _verify_structure(farm_plot: Node) -> void:
 	_verify_node_transform(farm_plot as Node2D, Vector2.ZERO, "FarmPlot")
 	for child_name in expected_names:
 		var sprite := farm_plot.get_node(NodePath(child_name)) as Sprite2D
-		_verify_node_transform(sprite, LAYER_POSITION, String(child_name))
 		_expect(not sprite.centered, "%s should not be centered" % child_name)
 		_expect(sprite.material == null, "%s should not have a material" % child_name)
 		_expect(sprite.texture_filter == CanvasItem.TEXTURE_FILTER_PARENT_NODE, "%s should inherit texture filtering" % child_name)
+	_verify_node_transform(farm_plot.get_node(^"PlotBase") as Sprite2D, BASE_POSITION, "PlotBase")
+	_verify_node_transform(farm_plot.get_node(^"CropShadow") as Sprite2D, EMPTY_CROP_POSITION, "CropShadow")
+	_verify_node_transform(farm_plot.get_node(^"CropColor") as Sprite2D, EMPTY_CROP_POSITION, "CropColor")
+	_expect((farm_plot.get_node(^"PlotBase") as Sprite2D).z_index == 0, "plot base z-order should remain zero")
+	_expect((farm_plot.get_node(^"CropShadow") as Sprite2D).z_index == 1, "crop shadow z-order should remain one")
+	_expect((farm_plot.get_node(^"CropColor") as Sprite2D).z_index == 2, "crop color z-order should remain two")
 	_expect((farm_plot.get_node(^"PlotBase") as Sprite2D).z_index < (farm_plot.get_node(^"CropShadow") as Sprite2D).z_index, "plot base should stack below crop shadow")
 	_expect((farm_plot.get_node(^"CropShadow") as Sprite2D).z_index < (farm_plot.get_node(^"CropColor") as Sprite2D).z_index, "crop shadow should stack below crop color")
 
@@ -64,6 +73,7 @@ func _verify_states(farm_plot: Node) -> void:
 	var original_root_position: Vector2 = farm_plot.position
 	var original_base_texture: Texture2D = plot_base.texture
 	var original_node_ids := [plot_base.get_instance_id(), crop_shadow.get_instance_id(), crop_color.get_instance_id()]
+	var original_z_indices := [plot_base.z_index, crop_shadow.z_index, crop_color.z_index]
 
 	_expect(farm_plot.initial_visual_state == FarmPlotScript.VisualState.EMPTY, "initial visual state should default to EMPTY")
 	_verify_state(farm_plot, FarmPlotScript.VisualState.EMPTY, false, "", "")
@@ -75,10 +85,11 @@ func _verify_states(farm_plot: Node) -> void:
 		_expect(plot_base.texture == original_base_texture, "plot base texture changed during cycle %d" % (cycle + 1))
 		_expect(plot_base.texture.resource_path == PLOT_BASE_PATH, "plot base path changed during cycle %d" % (cycle + 1))
 		_expect([plot_base.get_instance_id(), crop_shadow.get_instance_id(), crop_color.get_instance_id()] == original_node_ids, "a visual node was recreated during cycle %d" % (cycle + 1))
+		_expect([plot_base.z_index, crop_shadow.z_index, crop_color.z_index] == original_z_indices, "visual z-order changed during cycle %d" % (cycle + 1))
 		_expect(plot_base.get_parent() == farm_plot and crop_shadow.get_parent() == farm_plot and crop_color.get_parent() == farm_plot, "a visual node was reparented during cycle %d" % (cycle + 1))
-		_verify_node_transform(plot_base, LAYER_POSITION, "PlotBase after cycle %d" % (cycle + 1))
-		_verify_node_transform(crop_shadow, LAYER_POSITION, "CropShadow after cycle %d" % (cycle + 1))
-		_verify_node_transform(crop_color, LAYER_POSITION, "CropColor after cycle %d" % (cycle + 1))
+		_verify_node_transform(plot_base, BASE_POSITION, "PlotBase after cycle %d" % (cycle + 1))
+		_verify_node_transform(crop_shadow, EMPTY_CROP_POSITION, "CropShadow after cycle %d" % (cycle + 1))
+		_verify_node_transform(crop_color, EMPTY_CROP_POSITION, "CropColor after cycle %d" % (cycle + 1))
 		_expect(plot_base.visible, "plot base should remain visible during cycle %d" % (cycle + 1))
 
 
@@ -91,10 +102,14 @@ func _verify_state(farm_plot: Node, state: FarmPlotScript.VisualState, crops_vis
 	var plot_base := farm_plot.get_node(^"PlotBase") as Sprite2D
 	var crop_shadow := farm_plot.get_node(^"CropShadow") as Sprite2D
 	var crop_color := farm_plot.get_node(^"CropColor") as Sprite2D
+	var crop_position := _expected_crop_position(state)
 	_verify_node_transform(farm_plot as Node2D, Vector2.ZERO, "FarmPlot in %s" % FarmPlotScript.VisualState.keys()[state])
-	_verify_node_transform(plot_base, LAYER_POSITION, "PlotBase in %s" % FarmPlotScript.VisualState.keys()[state])
-	_verify_node_transform(crop_shadow, LAYER_POSITION, "CropShadow in %s" % FarmPlotScript.VisualState.keys()[state])
-	_verify_node_transform(crop_color, LAYER_POSITION, "CropColor in %s" % FarmPlotScript.VisualState.keys()[state])
+	_verify_node_transform(plot_base, BASE_POSITION, "PlotBase in %s" % FarmPlotScript.VisualState.keys()[state])
+	_verify_node_transform(crop_shadow, crop_position, "CropShadow in %s" % FarmPlotScript.VisualState.keys()[state])
+	_verify_node_transform(crop_color, crop_position, "CropColor in %s" % FarmPlotScript.VisualState.keys()[state])
+	_expect(crop_shadow.position == crop_color.position, "crop shadow and color should share a position in %s" % FarmPlotScript.VisualState.keys()[state])
+	_expect(crop_shadow.position.y == SOIL_CONTACT_Y and crop_color.position.y == SOIL_CONTACT_Y, "crop pair y-coordinate should remain -232.5 in %s" % FarmPlotScript.VisualState.keys()[state])
+	_expect(plot_base.z_index == 0 and crop_shadow.z_index == 1 and crop_color.z_index == 2, "visual z-order should remain fixed in %s" % FarmPlotScript.VisualState.keys()[state])
 	_expect(farm_plot.get_visual_state() == state, "get_visual_state should return %s" % FarmPlotScript.VisualState.keys()[state])
 	_expect(plot_base.visible, "plot base should be visible in %s" % FarmPlotScript.VisualState.keys()[state])
 	_expect(plot_base.texture != null and plot_base.texture.resource_path == PLOT_BASE_PATH, "plot base texture should remain exact in %s" % FarmPlotScript.VisualState.keys()[state])
@@ -106,6 +121,16 @@ func _verify_state(farm_plot: Node, state: FarmPlotScript.VisualState, crops_vis
 	else:
 		_expect(crop_shadow.texture == null, "crop shadow texture should be empty in EMPTY")
 		_expect(crop_color.texture == null, "crop color texture should be empty in EMPTY")
+
+
+func _expected_crop_position(state: FarmPlotScript.VisualState) -> Vector2:
+	match state:
+		FarmPlotScript.VisualState.GROWING:
+			return GROWING_CROP_POSITION
+		FarmPlotScript.VisualState.READY:
+			return READY_CROP_POSITION
+		_:
+			return EMPTY_CROP_POSITION
 
 
 func _verify_node_transform(node: Node2D, expected_position: Vector2, description: String) -> void:
